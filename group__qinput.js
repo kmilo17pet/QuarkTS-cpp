f140521730cf4a1b385846611f13d89d4724330e
kmilo17pet <kmilo17pet@users.noreply.github.com>
2024-06-05 15:20:57 +0000

    deploy: 129d7a40f3b405ddd6362d58a3a93146dcfed9d0

--- group__qinput.js
+++ group__qinput.js
@@ -7,11 +7,11 @@ var group__qinput =
       [ "channel", "classq_o_s_1_1input_1_1channel.html#ac394f68da414dfe8401700f25acd9058", null ],
       [ "getChannel", "classq_o_s_1_1input_1_1channel.html#afa6ca68a435f92ec6e612fe1d77bd0f9", null ],
       [ "getChannelType", "classq_o_s_1_1input_1_1channel.html#a76a41d1fc714232924865f00715e84ca", null ],
-      [ "getMultiPressCount", "classq_o_s_1_1input_1_1channel.html#ad0db667d1539e2c7ade6d58045a46edf", null ],
+      [ "getPulsationCount", "classq_o_s_1_1input_1_1channel.html#ae6974ca184f46a814b7cd96353a14a45", null ],
       [ "getUserData", "classq_o_s_1_1input_1_1channel.html#a842c0454b8b0355a4fc2a7b3ad9fac77", null ],
       [ "isShared", "classq_o_s_1_1input_1_1channel.html#a6b114e28bce381a3cb516eb5bf40d1ec", null ],
       [ "setChannel", "classq_o_s_1_1input_1_1channel.html#afe9029d08ecd265b09bb44546dd384e7", null ],
-      [ "setMultiPressInterval", "classq_o_s_1_1input_1_1channel.html#ac5f070e1bdf18c7d5ac579727ce30d49", null ],
+      [ "setPulsationInterval", "classq_o_s_1_1input_1_1channel.html#a5065ea5d2fbb49dd02e7d3605b88db0a", null ],
       [ "setUserData", "classq_o_s_1_1input_1_1channel.html#af17c9dbff0da8b8a4870e3a0908f30be", null ],
       [ "watcher", "classq_o_s_1_1input_1_1channel.html#ab1ffdab9f4319520dcf8416846fe276b", null ]
     ] ],
