bb83f686aad355e17ccd50b186f976edd0c1b99c
camilo <juancamilo.gomezcadavid@sensiaglobal.com>
2024-06-07 12:45:19 -0500

    bump to 1.6.4

--- src/QuarkTS.h
+++ src/QuarkTS.h
@@ -1,7 +1,7 @@
 /*!
  * @file QuarkTS.h
  * @author J. Camilo Gomez C.
- * @version 1.6.3
+ * @version 1.6.4
  * @note This file is part of the QuarkTS++ distribution.
  * @brief Global inclusion header
  **/
@@ -41,8 +41,8 @@ This file is part of the QuarkTS++ OS distribution.
 #ifndef QOS_CPP_H
 #define QOS_CPP_H
 
-#define QUARKTS_CPP_VERSION         "1.6.3"
-#define QUARKTS_CPP_VERNUM          ( 163u )
+#define QUARKTS_CPP_VERSION         "1.6.4"
+#define QUARKTS_CPP_VERNUM          ( 164u )
 #define QUARKTS_CPP_CAPTION         "QuarkTS++ OS " QUARKTS_CPP_VERSION
 
 #include "config/config.h"
@@ -70,7 +70,7 @@ namespace qOS {
         constexpr const uint8_t number = QUARKTS_CPP_VERNUM;
         constexpr const uint8_t mayor = 1U;
         constexpr const uint8_t minor = 6U;
-        constexpr const uint8_t rev = 3U;
+        constexpr const uint8_t rev = 4U;
     }
     namespace product {
         constexpr const char* author = "J. Camilo Gomez C.";
@@ -84,7 +84,7 @@ namespace qOS {
     }
 
     namespace build {
-        constexpr const uint32_t number = 4127;
+        constexpr const uint32_t number = 4128;
         constexpr const char* date = __DATE__;
         constexpr const char* time = __TIME__;
         constexpr const char* std = "c++11";
